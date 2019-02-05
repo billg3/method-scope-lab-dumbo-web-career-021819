c8a97432d35c600e5a20e98ad7b7ab427e3e9eb8
Bill G <gbilly3@gmail.com>
2019-02-05 21:49:41 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,10 @@ require_relative "../lib/catch_phrase.rb"
 require_relative "../lib/rescue_princess_peach.rb"
 require 'pry'
 
-
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+require "spec_helper"
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
 end
